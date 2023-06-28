3f076ed1407b9cd86cf45b99a4af8572b409e34c
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-06-28 11:36:07 +0300

    add fetch js & add variable js

---
+++ src/utils/constant.js
@@ -0,0 +1,36 @@
+import AiFillHome from 'react-icons/ai';
+import FaMusic from 'react-icons/fa';
+import BsCodeSlash from 'react-icons/bs';
+import MdOutlineOndemandVideo from 'react-icons/md';
+import MdSportsHandball from 'react-icons/md';
+import MdOutlineLiveTv from 'react-icons/md';
+import IoSchoolSharp from 'react-icons/io';
+import MdFaceRetouchingNatural from 'react-icons/md';
+import MdOutlineCheckroom from 'react-icons/md';
+import MdOutlineGraphicEq from 'react-icons/md';
+import MdOutlineTheaterComedy from 'react-icons/md';
+import MdFitnessCenter from 'react-icons/md';
+import FaBitcoin from 'react-icons/fa';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <AiFillHome />, },
+  { name: 'JS Mastery', icon: <BsCodeSlash />, },
+  { name: 'Coding', icon: <BsCodeSlash />, },
+  { name: 'ReactJS', icon: <BsCodeSlash />, },
+  { name: 'NextJS', icon: <BsCodeSlash />, },
+  { name: 'Music', icon: <FaMusic /> },
+  { name: 'Education', icon: <IoSchoolSharp />, },
+  { name: 'Podcast', icon: <MdOutlineGraphicEq />, },
+  { name: 'Movie', icon: <MdOutlineOndemandVideo />, },
+  { name: 'Gaming', icon: <MdSportsHandball />, },
+  { name: 'Live', icon: <MdOutlineLiveTv />, },
+  { name: 'Sport', icon: <MdFitnessCenter />, },
+  { name: 'Fashion', icon: <MdOutlineCheckroom />, },
+  { name: 'Beauty', icon: <MdFaceRetouchingNatural />, },
+  { name: 'Comedy', icon: <MdOutlineTheaterComedy />, },
+  { name: 'Gym', icon: <MdFitnessCenter />, },
+  { name: 'Crypto', icon: <FaBitcoin />, },
+];
+
