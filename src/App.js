eda25ad68587cf9908b8e53bd13e8b70fc228d84
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-06-28 11:34:21 +0300

    add style & add react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Feed,ChannelDetail,VideoDetail,SearchFeed,Navbar,Sidebar } from "./components/index"
+import "./index.css"
+import "./reset.css"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
+      <Sidebar />
         <Routes>
-          <Route />
+          <Route path="/" exacth element={<Feed />}/>
+          <Route path="/videoDetail" exacth element={<VideoDetail />}/>
+          <Route path="/channelDetail" exacth element={<ChannelDetail />}/>
+          <Route path="/searchFeed" exacth element={<SearchFeed />}/>
         </Routes>
       </BrowserRouter>
     </div>
