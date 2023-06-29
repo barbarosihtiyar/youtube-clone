5e6e8f22a3cc4eb5922334dd1ce51a7fd6978973
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-06-29 14:18:43 +0300

    get api & setVideos & setCategory

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Videos from './Videos'
+import { fetchFromApi } from '../utils/fetchFromApı'
 
 const Feed = () => {
+const [selectedCategory,setSelectedCategory] = useState('New')
+const [videos, setVideos] = useState(null);
+
+useEffect(() => {
+  
+  fetchFromApi(`search`).then((data) => setVideos(data.items))
+  console.log(videos)
+  }, [selectedCategory]);
+  
   return (
-    <div></div>
+    <div  className='ml-64 mt-4'>
+      <Videos videos={videos} name={"New"}/>
+    </div>
   )
 }
 
