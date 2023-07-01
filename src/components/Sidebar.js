9be6d67bd9a7d1a078b84a6dc6fa221caf42ed87
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-07-01 12:41:49 +0300

    conditions according selectedCategory & get api val & tailwind css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
 import { categories } from "../utils/constant";
+import { useMainContext } from '../contexts/context';
 
 const Sidebar = () => {
-  const [clickedCategory,setClickedCategory] = useState(0)
+  const {selectedCategory,setSelectedCategory} = useMainContext();
 
-  const changeClicked = (key) => {
-    setClickedCategory(key)
-    console.log(clickedCategory)
-  }
+  console.log(selectedCategory)
   return (
     <div className="fixed h-full overflow-hidden hover:overflow-auto w-56 border-r-1 border-slate-400">
       {categories.map((category, index) => (
-        <div key={index} onClick={() => changeClicked(index)}>
+        <div key={index} onClick={() => setSelectedCategory(category.name)}>
+          {selectedCategory === category.name ? 
+            <div className="flex gap-x-2 ms-2 px-3 py-2 my-4 cursor-pointer bg-red-600 rounded-full w-5/6  text-white ">
+            <span className="text-white categoryIcon">{category.icon}</span>
+            <span>{category.name}</span>
+          </div>
+          :
           <div className="flex gap-x-2 ms-2 px-3 py-2 my-4 cursor-pointer hover:bg-red-600 rounded-full w-5/6  hover:text-white sideBar">
             <span className="text-red-600 categoryIcon">{category.icon}</span>
             <span>{category.name}</span>
-          </div>
+          </div>}
         </div>
       ))}
     </div>
