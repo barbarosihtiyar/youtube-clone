4c56abfc14db90aa855c86ac8395545c6e466e7d
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-07-01 12:43:12 +0300

    get props & conditions & tailwind

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { TiTick } from './index'
 
-const VideoCard = () => {
+const VideoCard = ({video}) => {
+  // console.log(video)
   return (
-    <div>VideoCard</div>
+    video.id.videoId && 
+      <div className="flex cursor-pointer">
+      <div className='flex flex-col'>
+        <img src={video?.snippet?.thumbnails?.high?.url} alt={video.id.videoId} width={300} />
+        <div className="bg-zinc-800 videoCard max-w-full">
+        {video.snippet.title.length > 28 ? 
+           <p className='mb-2'>{video.snippet.title.slice(0, 28)}...</p>
+           : <p className='mb-2'>{video.snippet.title}</p>}
+         <div className="flex items-center gap-x-1">
+         <p>{video.snippet.channelTitle.slice(0, 28)}</p>
+         <TiTick />
+         </div>
+        </div>
+    </div>
+    </div>
   )
 }
 
