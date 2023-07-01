19f8838346beaef4d04cd34de455fe2145dc7090
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-07-01 12:40:15 +0300

    channel card component & get api val & tailwind css

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { TiTick } from './index'
 
-const ChannelCard = () => {
+const ChannelCard = ({channelDetail}) => {
+    console.log(channelDetail)
   return (
-    <div>ChannelCard</div>
+    <div className="flex cursor-pointer mt-8 justify-center" style={{width:"300px"}}>
+      <div className='flex flex-col'>
+        <img className='rounded-full' src={channelDetail?.snippet?.thumbnails?.high?.url} alt={channelDetail.id.channelDetailId} width={200} />
+        <div className="channelDetailCard max-w-full">
+         <div className="flex items-center gap-x-1 channelCard justify-center">
+         <p>{channelDetail.snippet.channelTitle.slice(0, 28)}</p>
+         <TiTick />
+         </div>
+        </div>
+    </div>
+    </div>
   )
 }
 
