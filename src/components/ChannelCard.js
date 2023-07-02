62a494c3a5151c715da5f1d231e2c8474990356f
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-07-02 20:41:31 +0300

    add conditions add style props

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { TiTick } from './index'
+import { Link } from 'react-router-dom'
 
-const ChannelCard = ({channelDetail}) => {
-    console.log(channelDetail)
+const ChannelCard = ({channelDetail,style}) => {
+    // console.log(channelDetail)
   return (
-    <div className="flex cursor-pointer mt-8 justify-center" style={{width:"300px"}}>
+    <div className="flex cursor-pointer mt-8 justify-center" style={style}>
+      <Link to={`channelDetail/${channelDetail?.snippet?.channelId}`}>
       <div className='flex flex-col'>
-        <img className='rounded-full' src={channelDetail?.snippet?.thumbnails?.high?.url} alt={channelDetail.id.channelDetailId} width={200} />
+        <img className='rounded-full' src={channelDetail?.snippet?.thumbnails?.high?.url} alt={channelDetail?.id?.channelDetailId} width={200} />
         <div className="channelDetailCard max-w-full">
          <div className="flex items-center gap-x-1 channelCard justify-center">
-         <p>{channelDetail.snippet.channelTitle.slice(0, 28)}</p>
+         <p>{channelDetail?.snippet?.title?.slice(0, 28)}</p>
          <TiTick />
          </div>
         </div>
     </div>
+      </Link>
     </div>
   )
 }
