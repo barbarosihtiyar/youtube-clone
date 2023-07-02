5281e8406d94e987ac926a8b88aa4e91244788c4
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-07-02 20:42:08 +0300

    get api & get params & send props

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchFromApi } from '../utils/fetchFromApi';
+import { ChannelCard, Videos } from "./index"
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail,setChannelDetail] = useState(null)
+  const [videos,setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromApi(`channels?part="snippet&id=${id}`)
+    .then((data) => setChannelDetail(data?.items[0]))
+
+    fetchFromApi(`search?channelId=${id}&part=snippet&order=date`)
+    .then((data) => setVideos(data?.items))
+  },[id])
+
+  console.log(channelDetail,videos)
   return (
-    <div>ChannelDetail</div>
+    <div style={{minHeight:"95vh"}}>
+      <div className='channelDetailLinear'></div>
+      <ChannelCard style={{marginTop:"-98px"}}  channelDetail={channelDetail}/>
+      <Videos videos={videos}/>
+    </div>
   )
 }
 
