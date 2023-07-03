31504d6c282ba627f495b038d92eb9080331f871
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-07-03 22:45:36 +0300

    get api & react hooks & context api & get context api & send props

--- src/components/SearchFeed.js
+++ src/components/SearchFeed.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Videos } from './index'
+import { useMainContext } from '../contexts/context';
+import { fetchFromApi } from '../utils/fetchFromApi';
+import { useParams } from 'react-router-dom';
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState(null);
+  const { selectedCategory,setSelectedCategory } = useMainContext();
+  const { searchTerm } = useParams();
+  
+  setSelectedCategory(searchTerm)
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+    // console.log(videos)
+  }, [selectedCategory]);
+
+  console.log(searchTerm)
   return (
-    <div>SearchFeed</div>
+    <div> <div className="ml-64 mt-4">
+    <Videos videos={videos} />
+  </div></div>
   )
 }
 
