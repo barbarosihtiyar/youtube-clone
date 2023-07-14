32ef9da3eb86ed566314d3813c43bd31f27135e6
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-07-14 23:36:57 +0300

    add params & getApi & add react-player & add icon & state

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+import { CheckCircle } from "@mui/icons-material";
+import { Videos } from "./";
+import { fetchFromApi } from "../utils/fetchFromApi";
+import { useParams } from "react-router-dom";
 
 const VideoDetail = () => {
-  return (
-    <div>VideoDetail</div>
-  )
-}
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+  const { id } = useParams();
 
-export default VideoDetail
+  useEffect(() => {
+    fetchFromApi(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    );
+
+    fetchFromApi(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
+    );
+  }, [id]);
+
+  return (
+    <div>
+      <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
+    </div>
+  );
+};
+
+export default VideoDetail;
