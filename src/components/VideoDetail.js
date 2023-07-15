a35b04c4aa9490fb47ae9a0faa4f06a0a1a213f1
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-07-15 15:44:51 +0300

    changed loader conditions & add link & add className & getApi

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,15 +2,14 @@ import React from "react";
 import { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 import { CheckCircle } from "@mui/icons-material";
-import { Videos } from "./";
+import { Loader, Videos } from "./";
 import { fetchFromApi } from "../utils/fetchFromApi";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
   const [videos, setVideos] = useState(null);
   const { id } = useParams();
-
   useEffect(() => {
     fetchFromApi(`videos?part=snippet,statistics&id=${id}`).then((data) =>
       setVideoDetail(data.items[0])
@@ -20,11 +19,25 @@ const VideoDetail = () => {
       (data) => setVideos(data.items)
     );
   }, [id]);
+  
+  if(!videoDetail?.snippet) return <Loader   />;
 
+  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetail;
   return (
-    <div>
-      <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
-    </div>
+    <>
+      <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
+      <Link to={`/channel/${channelId}`}>
+      <span>{title}</span>
+      </Link>
+      <Link to={`/channel/${channelId}`}>
+      <span> {channelTitle}</span>
+      </Link>
+      <span> {parseInt(viewCount).toLocaleString()} views</span>
+      <span> {parseInt(likeCount).toLocaleString()} likes</span>
+      <div>
+      <Videos videos={videos} direction="column" />
+      </div>
+    </>
   );
 };
 
