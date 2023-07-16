28054262bbca8051deb5b65c7b52fc05252541be
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-07-16 20:18:29 +0300

    add style

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -8,7 +8,7 @@ const ChannelCard = ({channelDetail,style}) => {
     <div className="flex cursor-pointer mt-8 justify-center" style={style}>
       <Link to={`channelDetail/${channelDetail?.snippet?.channelId}`}>
       <div className='flex flex-col'>
-        <img className='rounded-full' src={channelDetail?.snippet?.thumbnails?.high?.url} alt={channelDetail?.id?.channelDetailId} width={200} />
+        <img className='rounded-full' src={channelDetail?.snippet?.thumbnails?.high?.url} alt={channelDetail?.id?.channelDetailId} style={{borderRadius:"10px"}} width={200} />
         <div className="channelDetailCard max-w-full">
          <div className="flex items-center gap-x-1 channelCard justify-center">
          <p>{channelDetail?.snippet?.title?.slice(0, 28)}</p>
